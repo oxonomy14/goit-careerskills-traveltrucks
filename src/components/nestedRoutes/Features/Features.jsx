1ff36c14d820e1b6e0adda2addbcacb3d63cfb46
oxonomy14 <oxonomy14@gmail.com>
2025-08-24 02:31:39 +0700

    v0231-240825

--- src/components/nestedRoutes/Features/Features.jsx
+++ src/components/nestedRoutes/Features/Features.jsx
@@ -1,11 +1,64 @@
 import css from "./Features.module.css";
 import BookingForm from "../../BookingForm/BookingForm";
+import CamperFeatures from "../../CamperFeatures/CamperFeatures";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchByIdCamper } from "../../../redux/operations";
+import { selectCamper} from "../../../redux/selectors";
+import { useParams } from "react-router-dom";
+import { useEffect } from 'react';
 
 const Features =() => {
+    const { id } = useParams(); // беремо id з URL
+    const dispatch = useDispatch();
+    const camper = useSelector(selectCamper);
+
+     useEffect(() => {
+            if (id) {
+                dispatch(fetchByIdCamper(id));
+            }
+        }, [id, dispatch]);
 
     return (<>
     <div className={css.wrapper}>
-        <div className={css.featuresContent}></div>
+        <div className={css.featuresContent}>
+            <div className={css.camperFeatures}>
+            <CamperFeatures item={camper}/>
+            </div>
+            <div>
+             
+                    <h3 className={css.featuresTitle}>Vehicle details</h3>
+             
+                <div className={css.featuresBox}>
+                    <ul className={css.featuresBoxList}>
+                        <li className={css.featuresBoxItem}> 
+                            <span>Form</span>
+                        <span>{camper.form}</span>
+                        </li>
+                            <li className={css.featuresBoxItem}> 
+                            <span>Length</span>
+                        <span>{camper.length}</span>
+                        </li>
+                           <li className={css.featuresBoxItem}> 
+                            <span>Width</span>
+                        <span>{camper.width}</span>
+                        </li>
+                         <li className={css.featuresBoxItem}> 
+                            <span>Height</span>
+                        <span>{camper.height}</span>
+                        </li>
+                           <li className={css.featuresBoxItem}> 
+                            <span>Tank</span>
+                        <span>{camper.tank}</span>
+                        </li>
+                        <li className={css.featuresBoxItem}> 
+                            <span>Consumption</span>
+                        <span>{camper.consumption}</span>
+                        </li>
+                    </ul>
+                    
+                </div>
+            </div>
+        </div>
         <div className={css.bookingForm}>
             <BookingForm/>
         </div>
--- src/components/nestedRoutes/Reviews/Reviews.jsx
+++ src/components/nestedRoutes/Reviews/Reviews.jsx
@@ -1,16 +1,56 @@
 import css from "./Reviews.module.css";
 import BookingForm from "../../BookingForm/BookingForm";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchByIdCamper } from "../../../redux/operations";
+import { selectCamper} from "../../../redux/selectors";
+import { useParams } from "react-router-dom";
+import { useEffect } from 'react';
 
 
 const Reviews =() => {
+      const { id } = useParams(); // беремо id з URL
+    const dispatch = useDispatch();
+    const camper = useSelector(selectCamper);
+
+     useEffect(() => {
+            if (id) {
+                dispatch(fetchByIdCamper(id));
+            }
+        }, [id, dispatch]);
 
     return (
-        <>  <div className={css.wrapper}>
-        <div className={css.reviewsContent}></div>
+         <div className={css.wrapper}>
+        <div className={css.reviewsContent}>
+<ul className={css.reviewsContentList}>
+     {camper.reviews.map((item, index) => (
+    <li className={css.reviewsContentItem} key={index}>
+        <div className={css.reviewsContentInfo}>
+            <div className={css.reviewsContentInfoAvatar}><span>{item.reviewer_name.charAt(0)}</span></div>
+            <div className={css.reviewsContentRating}>
+                <p className={css.reviewsContentName}>{item.reviewer_name}</p>
+<div className={css.stars}>
+  {[...Array(5)].map((_, i) => (
+    <svg
+      key={i}
+      width={16}
+      height={16}
+      className={i < item.reviewer_rating ? css.IconStarFilled : css.IconStarEmpty}
+    >
+      <use xlinkHref="/icon/sprite.svg#icon-star"></use>
+    </svg>
+  ))}
+</div>
+                
+            </div>
+        </div>
+        <p className={css.reviewsContentItemDesc}>{item.comment}</p>
+    </li>))}
+</ul>
+        </div>
         <div className={css.bookingForm}>
             <BookingForm/>
         </div>
-    </div></>
+    </div>
     );
 
 };
